8043381a1dad58dbc49906bc107b5c69027d8d32
maryam jafari <maryamjafari12175@gmail.com>
2025-01-29 18:26:01 +0330

    first step redux project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Provider } from 'react-redux'
+import {store} from "./redux/store"
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <Provider store={store}>
+      <div> app</div>
+    </Provider>
+  )
+}
+
+export default App
---
+++ src/redux/cartSlice.ts
@@ -0,0 +1,45 @@
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+
+type CartItem = {
+  id: string;
+  title: string;
+  price: number;
+  qunatity: number;
+};
+
+type CartState = {
+  items: CartItem[];
+};
+
+const initialState: CartState = {
+  items: [],
+};
+export const cartSlice = createSlice({
+  name: " cart",
+  initialState,
+  reducers: {
+    addToCart(
+      state,
+      action: PayloadAction<{ id: string; title: string; price: number }>
+    ) {
+      const ItemIndex = state.items.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      if (ItemIndex >= 0) {
+        state.items[ItemIndex].qunatity++;
+      } else {
+        state.items.push({ ...action.payload, qunatity: 1 });
+      }
+    },
+    removeFromCart(state, action: PayloadAction<string>) {
+      const itemIndex = state.items.findIndex(
+        (item) => item.id === action.payload
+      );
+      if (state.items[itemIndex].qunatity === 1) {
+        state.items.splice(itemIndex, 1);
+      } else {
+        state.items[itemIndex].qunatity--;
+      }
+    },
+  },
+});
